b495e6909786a466eff1809aebec27a9be1f9b6f
Ryan Lelek <ryanlelek@gmail.com>
2014-12-14 14:05:09 -0800

    Module: Adding otto-request v0.1.0

--- test/index.test.js
+++ test/index.test.js
@@ -25,6 +25,10 @@ describe('Module Exports', function () {
     subject.should.have.property('errors').and.be.type('object');
   });
 
+  it('should export request as object', function () {
+    subject.should.have.property('request').and.be.type('object');
+  });
+
   it('should export authentication as object', function () {
     subject.should.have.property('authentication').and.be.type('object');
   });
